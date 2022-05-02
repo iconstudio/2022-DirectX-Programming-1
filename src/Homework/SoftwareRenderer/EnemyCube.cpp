d9c2dac65eabfb26b951b064f260fce8e3669c9c
Iconer <yoyofa2@hotmail.com>
2022-05-03 03:08:14 +0900

    Update EnemyCube.cpp

--- src/Homework/SoftwareRenderer/EnemyCube.cpp
+++ src/Homework/SoftwareRenderer/EnemyCube.cpp
@@ -1,2 +1,12 @@
 #include "stdafx.hpp"
 #include "EnemyCube.hpp"
+
+EnemyCube::EnemyCube(GameScene& scene)
+	: EnemyCube(Scene, XMFLOAT3(0, 0, 0))
+{}
+
+EnemyCube::EnemyCube(GameScene & scene, const XMFLOAT3& pos)
+	: Enemy(Scene, pos)
+{
+	InitHealth(10.0f);
+}
