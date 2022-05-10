17da94fe15d57da0a8d6d5c9e0225af47a3d06e9
Iconer <yoyofa2@hotmail.com>
2022-05-10 10:51:24 +0900

    렌더링 조각에 생성자 추가

--- src/Homework/SoftwareRenderer/Fragment.cpp
+++ src/Homework/SoftwareRenderer/Fragment.cpp
@@ -1,3 +1,16 @@
 #include "stdafx.hpp"
 #include "Fragment.hpp"
 
+CFragment::CFragment(const XMFLOAT3& begin, const XMFLOAT3& end, COLORREF col)
+{
+	start = begin;
+	dest = end;
+	Colour = col;
+}
+
+CFragment::CFragment(XMFLOAT3&& begin, XMFLOAT3&& end, COLORREF col)
+{
+	start = std::forward<XMFLOAT3>(begin);
+	dest = std::forward<XMFLOAT3>(end);
+	Colour = col;
+}
--- src/Homework/SoftwareRenderer/Fragment.hpp
+++ src/Homework/SoftwareRenderer/Fragment.hpp
@@ -4,6 +4,15 @@
 class CFragment
 {
 public:
+	CFragment() = default;
+	CFragment(const CFragment&) = default;
+	CFragment& operator=(const CFragment&) = default;
+	CFragment(CFragment&&) = default;
+	CFragment& operator=(CFragment&&) = default;
+
+	CFragment(const XMFLOAT3& begin, const XMFLOAT3& end, COLORREF col = 0);
+	CFragment(XMFLOAT3&& begin, XMFLOAT3&& end, COLORREF col = 0);
+
 	// Ω√¿€ x, y, z ¡¬«•
 	XMFLOAT3 start;
 
