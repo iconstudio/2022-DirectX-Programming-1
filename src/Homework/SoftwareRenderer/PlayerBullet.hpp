38e1bac334e71c90170e214692702a16cfd2a6e6
Iconer <yoyofa2@hotmail.com>
2022-05-11 16:41:41 +0900

    플레이어 총알의 고유 태그 추가

--- src/Homework/SoftwareRenderer/PlayerBullet.hpp
+++ src/Homework/SoftwareRenderer/PlayerBullet.hpp
@@ -8,14 +8,13 @@ public:
 	PlayerBullet();
 	PlayerBullet(const XMFLOAT3& pos);
 
-	virtual constexpr GOBJECT_TAGS GetTag() const { return GOBJECT_TAGS::PLAYER_BULLET; }
+	constexpr GOBJECT_TAGS GetTag() const { return GOBJECT_TAGS::PLAYER_BULLET; }
 
 	void SetParent(Player* parent);
 	void SetDamage(const float value);
 
 	void Ready();
-
-	virtual void Update(float elapsed_time);
+	void Update(float elapsed_time);
 
 	void OnCollisionWall(const XMFLOAT3& reflection);
 	void OnCollision(GameObject* other);
