f273450c105dfffca50230d54b550f98ac783cdb
Iconer <yoyofa2@hotmail.com>
2022-05-11 15:52:55 +0900

    정적인 객체에 위치 생성자 추가

--- src/Homework/SoftwareRenderer/GameStaticObject.cpp
+++ src/Homework/SoftwareRenderer/GameStaticObject.cpp
@@ -1,2 +1,14 @@
 #include "stdafx.hpp"
 #include "GameStaticObject.hpp"
+
+GameStaticObject::GameStaticObject()
+	: GameEntity()
+{}
+
+GameStaticObject::GameStaticObject(const XMFLOAT3& position)
+	: GameEntity(position)
+{}
+
+GameStaticObject::GameStaticObject(XMFLOAT3&& position)
+	: GameEntity(std::forward<XMFLOAT3>(position))
+{}
--- src/Homework/SoftwareRenderer/GameStaticObject.hpp
+++ src/Homework/SoftwareRenderer/GameStaticObject.hpp
@@ -5,6 +5,10 @@
 class GameStaticObject : public GameEntity
 {
 public:
+	GameStaticObject();
+	GameStaticObject(const XMFLOAT3& position);
+	GameStaticObject(XMFLOAT3&& position);
+
 	virtual constexpr GOBJECT_TAGS GetTag() const { return GOBJECT_TAGS::STATIC; }
 
 	virtual constexpr bool IsStatic() const override { return true; }
