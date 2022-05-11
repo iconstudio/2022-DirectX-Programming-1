2c85f1005d4924ee0a78d5aa955e2574ad21465a
Iconer <yoyofa2@hotmail.com>
2022-05-11 16:41:24 +0900

    파티클의 지속 시간 설정

--- src/Homework/SoftwareRenderer/GameParticle.cpp
+++ src/Homework/SoftwareRenderer/GameParticle.cpp
@@ -1,2 +1,23 @@
 #include "stdafx.hpp"
 #include "GameParticle.hpp"
+
+GameParticle::GameParticle(XMFLOAT3&& position, XMFLOAT3&& dir, float speed, float duration)
+	: GameEntity(std::forward<XMFLOAT3>(position))
+	, Direction(dir), Speed(speed), Friction(0.0f)
+	, isKilled(false), myLifetime(duration), myDuration(duration)
+{}
+
+GameParticle::~GameParticle()
+{}
+
+void GameParticle::Update(float elapsed_time)
+{
+	if (0 < myLifetime)
+	{
+		myLifetime -= elapsed_time;
+	}
+	else
+	{
+		isKilled = true;
+	}
+}
--- src/Homework/SoftwareRenderer/GameParticle.hpp
+++ src/Homework/SoftwareRenderer/GameParticle.hpp
@@ -5,5 +5,18 @@
 class GameParticle : public GameEntity
 {
 public:
+	GameParticle(XMFLOAT3&& position, XMFLOAT3&& dir, float speed, float duration);
+	virtual ~GameParticle();
+
 	virtual constexpr GOBJECT_TAGS GetTag() const { return GOBJECT_TAGS::NONE; }
+
+	virtual void Update(float elapsed_time);
+
+	const XMFLOAT3 Direction;
+	float Speed;
+	float Friction;
+
+	bool isKilled;
+	float myLifetime;
+	float myDuration;
 };
