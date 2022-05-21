a83bf4352ee6574dcbda387d7225e8d89c0cb996
Iconer <yoyofa2@hotmail.com>
2022-05-21 22:42:24 +0900

    중간과제 용 솔루션 생성

---
+++ src/Midterm 2022-1/Midterm 2022-1/stdafx.hpp
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "targetver.h"
+
+#pragma comment(lib, "winmm.lib")
+#define WIN32_LEAN_AND_MEAN 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include <D3Dcompiler.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include <memory>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+
+using std::shared_ptr;
+using std::unique_ptr;
+using std::make_shared;
+using std::make_unique;
+using std::string;
+using Filepath = std::filesystem::path
