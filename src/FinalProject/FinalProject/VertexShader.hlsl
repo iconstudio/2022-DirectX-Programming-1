e74721728a96889bcf00fb7b1dc9f0790478163f
Iconer <yoyofa2@hotmail.com>
2022-06-03 07:32:50 +0900

    정점 쉐이더 추가

--- src/FinalProject/FinalProject/VertexShader.hlsl
+++ src/FinalProject/FinalProject/VertexShader.hlsl
@@ -1,6 +1,10 @@
-
-
 struct VS_LIGHTING_INPUT
+{
+    float3 position : POSITION;
+    float3 normal : NORMAL;
+};
+
+struct VS_DIFFUSED_INPUT
 {
     float3 position : POSITION;
     float3 normal : NORMAL;
@@ -11,7 +15,7 @@ struct VS_LIGHTING_OUTPUT
     float4 position : SV_POSITION;
     float3 positionW : POSITION;
     float3 normalW : NORMAL;
-	float4 color : COLOR;
+    float4 color : COLOR;
 };
 
 cbuffer cbCameraInfo : register(b1)
@@ -34,13 +38,13 @@ VS_LIGHTING_OUTPUT main(VS_LIGHTING_INPUT input)
     output.normalW = mul(input.normal, (float3x3) gmtxGameObject);
     output.positionW = (float3) mul(float4(input.position, 1.0f), gmtxGameObject);
     output.position = mul(mul(float4(output.positionW, 1.0f), gmtxView), gmtxProjection);
-	output.normalW = normalize(output.normalW);
-	output.color = Lighting(output.positionW, output.normalW);
+    output.normalW = normalize(output.normalW);
+    output.color = Lighting(output.positionW, output.normalW);
 	
     return output;
 }
 
-float4 main( float4 pos : POSITION ) : SV_POSITION
+float4 main(float4 pos : POSITION) : SV_POSITION
 {
-	return pos;
+    return pos;
 }
