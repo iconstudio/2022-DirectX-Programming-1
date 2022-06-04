09bcadcd9c186060732172bbbc1a87ae7cea23b3
Iconer <yoyofa2@hotmail.com>
2022-06-04 14:01:45 +0900

    Update VertexShader.hlsl

--- src/FinalProject/FinalProject/shaders/VertexShader.hlsl
+++ src/FinalProject/FinalProject/shaders/VertexShader.hlsl
@@ -5,7 +5,8 @@ VS_LIGHTING_OUTPUT main(VS_LIGHTING_INPUT input)
 	VS_LIGHTING_OUTPUT output;
 
 	output.normalW = mul(input.normal, (float3x3)gmtxGameObject);
-	output.positionW = (float3)mul(float4(input.position, 1.0f), gmtxGameObject);
+	output.positionW = mul(float4(input.position, 1.0f), gmtxGameObject).xyz;
+	
 	output.position = mul(mul(float4(output.positionW, 1.0f), gmtxView), gmtxProjection);
 #ifdef _WITH_VERTEX_LIGHTING
 	output.normalW = normalize(output.normalW);
