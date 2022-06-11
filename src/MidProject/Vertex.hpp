ecdb74f7c387bbd0c87755e10b7b4ca0d2006fd1
Iconer <yoyofa2@hotmail.com>
2022-06-11 14:07:11 +0900

    정점 클래스 수정

--- src/MidProject/Vertex.hpp
+++ src/MidProject/Vertex.hpp
@@ -10,30 +10,29 @@ public:
 	CVertex(XMFLOAT3&& position);
 	virtual ~CVertex();
 
-	const XMFLOAT3 myPosition;
+	XMFLOAT3 myPosition;
 };
 
 class CDiffusedVertex : public CVertex
 {
 public:
-	CDiffusedVertex(XMFLOAT4 colour);
-	CDiffusedVertex(float x, float y, float z, XMFLOAT4 colour);
-	CDiffusedVertex(float position[3], XMFLOAT4 colour);
-	CDiffusedVertex(const XMFLOAT3& position, XMFLOAT4 colour);
-	CDiffusedVertex(XMFLOAT3&& position, XMFLOAT4 colour);
+	CDiffusedVertex(const CVertex& position);
+	CDiffusedVertex(CVertex&& position);
+	CDiffusedVertex(const XMFLOAT4 colour);
+	CDiffusedVertex(const CVertex& position, const XMFLOAT4 colour);
+	CDiffusedVertex(CVertex&& position, const XMFLOAT4 colour);
 	virtual ~CDiffusedVertex();
 
-	const XMFLOAT4 myColour;
+	XMFLOAT4 myColour;
 };
 
 class CLightenVertex : public CVertex
 {
 public:
-	CLightenVertex(const XMFLOAT3 normal);
-	CLightenVertex(float x, float y, float z, const XMFLOAT3 normal);
-	CLightenVertex(float position[3], const XMFLOAT3 normal);
-	CLightenVertex(const XMFLOAT3& position, const XMFLOAT3 normal);
-	CLightenVertex(XMFLOAT3&& position, const XMFLOAT3 normal);
+	CLightenVertex(const CVertex& position);
+	CLightenVertex(CVertex&& position);
+	CLightenVertex(const CVertex& position, const XMFLOAT3 normal);
+	CLightenVertex(CVertex&& position, const XMFLOAT3 normal);
 	virtual ~CLightenVertex();
 
 	XMFLOAT3 myNormal;
