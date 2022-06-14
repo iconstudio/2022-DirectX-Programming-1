2572f5f4454aa52d847c19bda3a0999f8ecc08f2
Iconer <yoyofa2@hotmail.com>
2022-06-14 20:34:45 +0900

    그래픽 자원의 폴더 추가

--- src/MidProject/Terrains.hpp
+++ src/MidProject/Terrains.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include "Object.h"
 #include "Mesh.h"
+#include "RawMesh.hpp"
 
 class Terrain : public GameObject
 {
 
 };
 
-class CTerrainMesh : public CMesh
+class CTerrainMesh : public CDiffusedMesh
 {
 public:
 
@@ -15,5 +16,16 @@ public:
 
 struct TerrainData
 {
+	TerrainData(const Filepath& image, int w, int size, XMFLOAT3 scale);
+	~TerrainData();
+
+	RawMesh Build() const;
+
+	BYTE* m_pHeightMapPixels;
+
+	int m_nWidth;
+	int m_nLength;
+	XMFLOAT3 m_xmf3Scale;
+
 
 };
